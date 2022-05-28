827e15ea222599b2f96d92432e5a78d7b91f2a56
rtavares <ricardotavaresit@gmail.com>
2022-05-28 16:50:41 +0100

    create validations rules

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,14 +1,31 @@
 <template>
   <div>
     <form>
-      <input type="text" placeholder="Name" v-model="userData.name"> {{userData.name}} <hr>
-      <input type="email" placeholder="E-Mail" v-model="userData.email"><hr>
-      <input type="number" placeholder="Idade" v-model.number="userData.age"><hr>
-      <input type="radio" name="sex" value="M" v-model="userData.sex"> Male
+      <input type="text" placeholder="Name" v-model="userData.name" />
+      {{ userData.name }}
+      <hr />
+      <input
+        type="email"
+        name="email"
+        v-validate="'required|email'"
+        placeholder="E-Mail"
+        v-model="userData.email"
+      />
+
+     <!--  <div v-bind:class="{ 'is-danger': errors.has('email') }"> -->
+      <div v-bind:class=" errors.has('email') ?  'is-danger' : 'is-success'">
+        <span v-show="errors.has('email')" class="help is-danger">{{
+          errors.first("email")
+        }}</span>
+      </div>
+      <hr />
+      <input type="number" placeholder="Idade" v-model.number="userData.age" />
+      <hr />
+      <input type="radio" name="sex" value="M" v-model="userData.sex" /> Male
       <span>|</span>
-      <input type="radio" name="sex" value="F" v-model="userData.sex"> Female
-      {{userData.sex}}
-     <hr>
+      <input type="radio" name="sex" value="F" v-model="userData.sex" /> Female
+      {{ userData.sex }}
+      <hr />
       <select v-model="userData.state">
         <option value="">select city</option>
         <option value="lb">Lisbon</option>
@@ -16,50 +33,63 @@
         <option value="av">Aveiro</option>
         <option value="vi">Viseu</option>
       </select>
-      <p v-if="userData.state">select City: {{userData.state}}</p>
-      <hr>
+      <p v-if="userData.state">select City: {{ userData.state }}</p>
+      <hr />
       <label for="agree">I agree with the terms of use</label>
-      <input type="checkbox" id="agree" v-model="userData.terms">
-      <hr>
+      <input type="checkbox" id="agree" v-model="userData.terms" />
+      <hr />
       <textarea cols="30" rows="10" v-model="userData.description"></textarea>
       <p>Version 1</p>
       <div class="text-formate">
-        {{userData.description}}
+        {{ userData.description }}
       </div>
-       <p>Version 2</p>
+      <p>Version 2</p>
       <pre>
-        {{userData.description}}
+        {{ userData.description }}
       </pre>
-      <hr>
+      <hr />
       <button typ="submit">Send</button>
     </form>
   </div>
 </template>
 
 <script>
-
 export default {
-  data(){
-    return{
-      userData:{
-        name:'',
-        email:'',
-        age:'',
-        sex:'',
-        state:'',
+  data() {
+    return {
+      userData: {
+        name: "",
+        email: "",
+        age: "",
+        sex: "",
+        state: "",
       },
       terms: true,
-      description:''
-    }
-  }
-}
-
+      description: "",
+    };
+  },
+};
 </script>
 
 
   <style scoped>
-  .text-formate{
+  .text-formate {
     white-space: pre;
   }
-</style>
 
+  .is-danger {
+    border: 1px solid rgb(179, 11, 11);
+  }
+  .is-danger p {
+    color: rgb(179, 11, 11);
+  }
+
+
+  .is-success {
+    border: 1px solid rgb(12, 123, 27);
+  }
+  .is-success p {
+    color: rgb(12, 123, 27);
+  }
+
+</style>
